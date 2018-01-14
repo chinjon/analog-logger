8da3556617f1c43fe28e1583dbc6e6b44eff1a03
Jonathan Chin <jonathankchin@gmail.com>
2018-01-14 15:06:29 -0500

    updated App.js to test if data is outputted via mock json

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 
 import CurrentRoll from './components/CurrentRoll';
+import mockData from './data/mockFilm.json';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mockData
+    }
+  }
+
   render() {
+    const { mockData } = this.state;
     return (
       <div className="App" id="root">
         <CurrentRoll />
+        <section>
+          <h3>Everything</h3>
+          <div>
+            { mockData.film.map(roll => 
+              <div key={roll.objectId}>
+                <div>Roll: {roll.objectId}</div>
+              </div>
+            )}
+          </div>
+        </section>
       </div>
     );
   }
