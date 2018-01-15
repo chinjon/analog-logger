eb5eb6c3f46b282ae4098a1e8bc8fadcb5b7aef4
Jonathan Chin <jonathankchin@gmail.com>
2018-01-14 20:26:45 -0500

    updated input component

--- src/components/subComponents/currentRollFrameInput.js
+++ src/components/subComponents/currentRollFrameInput.js
@@ -5,6 +5,10 @@ const CurrentRollFrameInput = () => {
         <section>
             <form>
                 <label>Enter Frame Data:</label>
+                <input type="text" placeholder="iso" />
+                <input type="text" placeholder="focal length" />
+                <input type="text" placeholder="aperture" />
+                <input type="text" placeholder="shutter speed" />
             </form>
         </section>
     )
