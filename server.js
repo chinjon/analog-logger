dac4f17a331ac42c9a5e62de1c8ae9ce669446e0
Jonathan Chin <jonathankchin@gmail.com>
2018-01-21 17:56:55 -0500

    added mongoose and mongodb connection

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const data = require('./data/mockFilm.json');
+const routes = require('./routes/routes');
+const mongoose = require('mongoose');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-const port = process.env.PORT || 5000;
 
+app.set('port', (process.env.PORT || 5000));
 
-app.get('/api/hello', (req, res) => {
-    res.send({
-        data
-    });
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+mongoose.Promise = Promise;
+
+const dbUri = process.env.MONGODB_URI || "mongodb://localhost/analog-logger";
+
+mongoose.connect(dbUri).then(() => console.log('connected to DB!')).catch((err) => console.log(err));
+
+app.use('/', routes);
+
+app.listen(app.get('port'), () => {
+    console.log(`Find the server at: http://localhost:${app.get('port')}/`); 
+  });
