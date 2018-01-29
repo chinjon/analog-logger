8ace9278eb8f37f411e7875051e4e443f85f0db6
Jonathan Chin <jonathankchin@gmail.com>
2018-01-28 22:59:54 -0500

    created new roll component and added form handling functions

--- client/src/components/subComponents/NewRoll.js
+++ client/src/components/subComponents/NewRoll.js
@@ -1,5 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const NewRoll = () => {
-    
+
+class NewRoll extends Component {
+    constructor() {
+        super();
+        this.state = {
+            film: {
+                brand: '',
+            
+            }
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        console.log(event.target.value)
+
+        this.setState({
+            film: {
+                brand: event.target.value
+            }
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log('ok')
+
+        fetch('/api/newroll', {
+            method: 'POST',
+            body: this.state,
+        });
+    }
+
+    render() {
+        return ( 
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" name="filmBrand" value={this.state.value} onChange={this.handleChange}></input>
+                    <button>Submit</button>
+                </form>
+        )
+    }
 }
+
+
+export default NewRoll;
