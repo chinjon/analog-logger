ea445e8c60faf7dbf6f72676b477a5f49d76a3ac
Jonathan Chin <jonathankchin@gmail.com>
2018-02-04 18:05:02 -0500

    wired up form with fetch api to send input to db

--- client/src/components/subComponents/NewRoll.js
+++ client/src/components/subComponents/NewRoll.js
@@ -5,39 +5,78 @@ class NewRoll extends Component {
     constructor() {
         super();
         this.state = {
-            film: {
-                brand: '',
+
+            brand: '',
+            model: '',
+            iso: '',
+            expCounter: '',
             
-            }
         }
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(event) {
-        console.log(event.target.value)
 
         this.setState({
-            film: {
-                brand: event.target.value
-            }
+              [event.target.name]: event.target.value
         })
+
+        console.log(this.state)
     }
 
     handleSubmit(event) {
         event.preventDefault();
         console.log('ok')
 
+        const newRoll = {
+            film: {
+                brand: this.state.brand,
+                model: this.state.model,  
+                iso: this.state.iso,  
+                expCount: this.state.expCount,  
+            }
+        }
+
         fetch('/api/newroll', {
             method: 'POST',
-            body: this.state,
+            body: JSON.stringify(newRoll)
         });
+
+        console.log(this.state)
     }
 
     render() {
         return ( 
                 <form onSubmit={this.handleSubmit}>
-                    <input type="text" name="filmBrand" value={this.state.value} onChange={this.handleChange}></input>
+                    <input 
+                        type="text" 
+                        name="brand"  
+                        value={this.state.brand} 
+                        onChange={this.handleChange}  
+                        placeholder="Film Brand"
+                    />
+                    <input 
+                        type="text" 
+                        name="model" 
+                        value={this.state.model} 
+                        onChange={this.handleChange} 
+                        placeholder="Film Model"
+                    />
+                    <input 
+                        type="text" 
+                        name="iso" 
+                        value={this.state.iso} 
+                        onChange={this.handleChange} 
+                        placeholder="Film Speed/ISO"
+                    />
+                    <input 
+                        type="number" 
+                        name="expCount" 
+                        value={this.state.expCount} 
+                        onChange={this.handleChange} 
+                        placeholder="Exposures"
+                    />
                     <button>Submit</button>
                 </form>
         )
