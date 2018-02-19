cb2179d83752fc79292dec1421da81bacab6086d
Jonathan Chin <jonathankchin@gmail.com>
2018-02-19 01:54:19 -0500

    drafting component to handle roll data

---
+++ client/src/components/subComponents/DataRoll.js
@@ -0,0 +1,13 @@
+import react, { Component } from 'react';
+
+class DataRoll extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            currentRoll: ''
+        }
+    }
+
+    
+}
