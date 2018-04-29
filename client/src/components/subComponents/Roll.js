910bfc7939a13d49f8be168dd8713c9f84c97ca6
Jonathan Chin <jonathankchin@gmail.com>
2018-04-29 16:49:18 -0400

    removed commented out code

--- client/src/components/subComponents/Roll.js
+++ client/src/components/subComponents/Roll.js
@@ -8,10 +8,7 @@ class Roll extends Component {
                     <span>Brand: {this.props.data.film.brand}</span> - 
                     <span>{this.props.data.film.model}</span> | 
                     <span>ISO: {this.props.data.film.iso}</span> 
-                    <button data-frame-id={this.props.data._id} onClick={this.props.deleteClick} >Delete</button></div>
-                {/* {this.props.data.frames.map(item => 
-                    <Frame key={item.number} data={item}/>
-                )} */}     
+                    <button data-frame-id={this.props.data._id} onClick={this.props.deleteClick}>Delete</button></div> 
             </div>
         )
     }
